dde062a645ba3dc9a825175b5b577436bc7af3bb
xieningjun <779016161@qq.com>
2025-05-07 14:04:07 +0800

    feat: MethodMeta改为使用builder模式创建

--- jdbc/src/main/java/org/venti/jdbc/meta/MetaParser.java
+++ jdbc/src/main/java/org/venti/jdbc/meta/MetaParser.java
@@ -10,7 +10,6 @@ import org.venti.jdbc.visitor.SelectVisitor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
