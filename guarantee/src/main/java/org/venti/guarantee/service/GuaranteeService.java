9d47fb36fb1e4d1890b96aa4be2f4a151a6731a4
xieningjun <779016161@qq.com>
2025-05-22 18:07:55 +0800

    feat: 新增批量查询功能

--- guarantee/src/main/java/org/venti/guarantee/service/GuaranteeService.java
+++ guarantee/src/main/java/org/venti/guarantee/service/GuaranteeService.java
@@ -5,10 +5,14 @@ import org.venti.guarantee.entity.RO.GetGuaranteeRO;
 import org.venti.guarantee.entity.VO.AddGuaranteeVO;
 import org.venti.guarantee.entity.VO.GetGuaranteeVO;
 
+import java.util.List;
+
 public interface GuaranteeService {
 
     AddGuaranteeVO addGuarantee(AddGuaranteeRO ro);
 
     GetGuaranteeVO getGuarantee(GetGuaranteeRO ro);
+
+    List<GetGuaranteeVO> getAllGuarantees();
 
 }
